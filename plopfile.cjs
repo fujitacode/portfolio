273a48d8a440ed95a2abdaa6c5a78a4e9d6da623
fujitacode <riot.jack.bbb@gmail.com>
2026-04-05 23:48:53 +0900

    style: :zap: コード整形 #1

--- plopfile.cjs
+++ plopfile.cjs
@@ -1,29 +1,29 @@
 module.exports = (plop) => {
-  plop.setGenerator("component", {
-    description: "Generate component files",
-    prompts: [
-      {
-        type: "input",
-        name: "name",
-        message: "Component name",
-      },
-    ],
-    actions: [
-      {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
-        templateFile: "plop-templates/component.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
-        templateFile: "plop-templates/component.stories.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/index.ts",
-        templateFile: "plop-templates/index.ts.hbs",
-      },
-    ],
-  });
+	plop.setGenerator("component", {
+		description: "Generate component files",
+		prompts: [
+			{
+				type: "input",
+				name: "name",
+				message: "Component name",
+			},
+		],
+		actions: [
+			{
+				type: "add",
+				path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
+				templateFile: "plop-templates/component.tsx.hbs",
+			},
+			{
+				type: "add",
+				path: "src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx",
+				templateFile: "plop-templates/component.stories.tsx.hbs",
+			},
+			{
+				type: "add",
+				path: "src/components/{{pascalCase name}}/index.ts",
+				templateFile: "plop-templates/index.ts.hbs",
+			},
+		],
+	});
 };
